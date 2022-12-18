b701e460613c620d1de224d0fc98ba7de28e2c74
Derrick Nyarangi <40804191+derrickgolden@users.noreply.github.com>
2022-12-18 13:27:34 +0300

    First project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 8000;
+
+const app = express()
+
+app.get('/golden/*',getHome)
+app.listen(PORT,()=>console.log("Listening on port", PORT))
+
+const {readFile, createReadStream, rmSync} = require('fs')
+const path = require('path')
+
+async function getHome(req,res){
+    const filePath = path.join(`${__dirname}/${req.params[0]}`)
+    createReadStream(filePath)
+        .on("error",() => notFound(req,res))
+        .pipe(res)
+}
+
+function notFound(req,res){
+    res.writeHead(404,{"Content-Type":"text/plain"})
+    res.end("Page Not Found")
+}
