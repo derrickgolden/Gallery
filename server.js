5cc2e109296973b68df9711641008f5d6b98282a
Derrick Nyarangi <40804191+derrickgolden@users.noreply.github.com>
2022-12-27 06:53:12 +0300

    status root

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 8000;
 
 const app = express()
 
+app.use('/',(req,res)=>{
+    res.sendStatus(200)
+})
 app.get('/golden/*',getHome)
 app.listen(PORT,()=>console.log("Listening on port", PORT))
 
