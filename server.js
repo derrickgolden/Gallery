4b99d34d171ac612759f7522123f99a7004b4f1e
Derrick Nyarangi <40804191+derrickgolden@users.noreply.github.com>
2022-12-27 10:01:33 +0300

    trying render

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const app = express()
 // app.use('/',(req,res)=>{
 //     res.sendStatus(200)
 // })
-app.get('/',getHome)
+app.get('/golden',getHome)
 app.listen(PORT,()=>console.log("Listening on port", PORT))
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-  })
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+//   })
 
 const {readFile, createReadStream, rmSync} = require('fs')
 const path = require('path')
 
-const filePath = ""
+// const filePath = ""
 async function getHome(req,res){
-    filePath = path.join(`${__dirname}/${req.params[0]}`)
+    const filePath = path.join(`${__dirname}/${req.params[0]}`)
     console.log(filePath)
     createReadStream(filePath)
         .on("error",() => notFound(req,res))
