2c1e43e7aecf29c702ef4e168498983c3dec3d2c
Derrick Nyarangi <40804191+derrickgolden@users.noreply.github.com>
2023-01-12 15:34:49 +0300

    lower image quality

--- gallery.js
+++ gallery.js
@@ -5,34 +5,34 @@ let grayDom =`
 <H3 class="art-type">BLACK AND WHITE DRAWING(GRAY)</H3>
 <div class="gallery-images">
     <img src="./drawings/gray/sm_betty_complete.JPG" alt="BLACK AND WHITE DRAWING">
-    <img src="./drawings/gray/DSCN0125.JPG" alt="BLACK AND WHITE DRAWING">
-    <img src="./drawings/gray/IMG_20210312_145251.jpg" alt="BLACK AND WHITE DRAWING">
-    <img src="./drawings/gray/IMG_20210709_152052.jpg" alt="BLACK AND WHITE DRAWING">
-    <img src="./drawings/gray/IMG_20210630_101035.jpg" alt="BLACK AND WHITE DRAWING">
-    <img src="./drawings/gray/IMG_20211220_164853 (1).jpg" alt="BLACK AND WHITE DRAWING">
+    <img src="./drawings/gray/sm_twin_kid_complete.JPG" alt="BLACK AND WHITE DRAWING">
+    <img src="./drawings/gray//sm_law_kid_comp.jpg" alt="BLACK AND WHITE DRAWING">
+    <img src="./drawings/gray/sm_cynthia_comp.jpg" alt="BLACK AND WHITE DRAWING">
+    <img src="./drawings/gray/sm_daisy_comp.jpg" alt="BLACK AND WHITE DRAWING">
+    <img src="./drawings/gray/sm_winnie_comp.jpg" alt="BLACK AND WHITE DRAWING">
 </div>
 <H3 class="art-type">BLACK AND WHITE PAINTING(GRAY)</H3>
 <div class="gallery-images">
-    <img src="./drawings/gray/IMG_20200922_064156.jpg" alt="BLACK AND WHITE  PAINTING">
-    <img src="./drawings/gray/IMG_20210325_080604.jpg" alt="BLACK AND WHITE  PAINTING">
-    <img src="./drawings/gray/IMG_20210102_110916.jpg" alt="BLACK AND WHITE  PAINTING">
-    <img src="./drawings/gray/IMG_20210603_105513.jpg" alt="BLACK AND WHITE  PAINTING">
-    <img class="landscape" src="./drawings/gray/IMG_20201202_083037.jpg" alt="BLACK AND WHITE  PAINTING">
-    <img class="landscape" src="./drawings/gray/IMG_20220903_110116 (1).jpg" alt="BLACK AND WHITE  PAINTING">
+    <img src="./drawings/gray/sm_dad_son_paint_complete.jpg" alt="BLACK AND WHITE PAINTING">
+    <img src="./drawings/gray/sm_couple_comp.jpg" alt="BLACK AND WHITE PAINTING">
+    <img src="./drawings/gray/sm_sun_face_paint_comp.jpg" alt="BLACK AND WHITE PAINTING">
+    <img src="./drawings/gray/sm_qatar_paint_comp.jpg" alt="BLACK AND WHITE PAINTING">
+    <img class="landscape" src="./drawings/gray/sm_nigeria_family_paint_comp.jpg" alt="BLACK AND WHITE PAINTING">
+    <img class="landscape" src="./drawings/gray/sm_SA_family_paint_comp.jpg" alt="BLACK AND WHITE PAINTING">
 </div>
 `
 
 let colDom =`
 <H3 class="art-type">COLORED PAINTINGS</H3>
 <div class="gallery-images">
-    <img src="./drawings/colored/IMG_20220425_193116.jpg" alt="COLORED PAINTING">
-    <img src="./drawings/colored/IMG_20220401_182307.jpg" alt="COLORED PAINTING">
-    <img src="./drawings/colored/IMG_20201123_150645.jpg" alt="COLORED PAINTING">
-    <img src="./drawings/colored/IMG_20220308_095729.jpg" alt="COLORED PAINTING">
-    <img src="./drawings/colored/IMG_20220830_105816.jpg" alt="COLORED PAINTING">
-    <img class="landscape" src="./drawings/colored/IMG_20200213_083804.jpg" alt="COLORED PAINTING">
-    <img class="landscape" src="./drawings/colored/IMG_20200306_163522.jpg" alt="COLORED PAINTING">
-    <img class="landscape" src="./drawings/colored/IMG-20221004-WA0003.jpg" alt="COLORED PAINTING">
+    <img src="./drawings/colored/sm_couple_col_comp.jpg" alt="COLORED PAINTING">
+    <img src="./drawings/colored/sm_prision_col_comp.jpg" alt="COLORED PAINTING">
+    <img src="./drawings/colored/sm_lady_col_inc.jpg" alt="COLORED PAINTING">
+    <img src="./drawings/colored/sm_leop_monkey_col.jpg" alt="COLORED PAINTING">
+    <img src="./drawings/colored/sm_cheetah_col.jpg" alt="COLORED PAINTING">
+    <img class="landscape" src="./drawings/colored/sm_lady1_col.jpg" alt="COLORED PAINTING">
+    <img class="landscape" src="./drawings/colored/sm_mzungu_family_col.jpg" alt="COLORED PAINTING">
+    <img class="landscape" src="./drawings/colored/sm_kinoti_wall_col.jpg" alt="COLORED PAINTING">
 </div>
 `
 let gallery0 = document.body.querySelector(".gallery-img")
